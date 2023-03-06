8dbb622a51b3a42614405f4da204dc9e7f6d3be7
Sho Hirose <sho.hirose@gmail.com>
2023-03-06 20:24:49 +0900

    refactor trapezoid

--- trapezoid.cpp
+++ trapezoid.cpp
@@ -2,21 +2,25 @@
 #include <mpi.h>
 
 #include <cstdio>
-#include <utility>
+
+struct Range {
+  Range(double low_, double high_) : low{low_}, high{high_} {}
+
+  double low;
+  double high;
+};
 
 /// @brief Integrates a function using the trapezoidal rule.
 /// @tparam F Function type to be integrated
-/// @param[in] x_low Lower limit of the integral interval
-/// @param[in] x_low Upper limit of the integral interval
+/// @param[in] rng Integral range
 /// @param[in] n Number of bins
 /// @param[in] f Function to be integrated
 /// @returns Definite integral
 template <typename F>
-double integrate_using_trapezoidal_rule(double x_low, double x_high, int n,
-                                        F&& f) {
-  const auto h = (x_high - x_low) / n;
+double integrate(Range rng, int n, F&& f) {
+  const auto h = (rng.high - rng.low) / n;
   double sum = 0;
-  double x0 = x_low;
+  double x0 = rng.low;
   double x1 = x0 + h;
   for (int i = 0; i < n; ++i) {
     const auto f0 = f(x0);
@@ -28,12 +32,18 @@ double integrate_using_trapezoidal_rule(double x_low, double x_high, int n,
   return sum;
 }
 
-/// @brief Computes the integral range of the current process
-std::pair<double, double> calc_my_range(int size, int rank) {
+/**
+ * @brief Gets the integral range of the current process
+ * 
+ * @param size Number of processes
+ * @param rank Process ID
+ * @return Integral range
+ */
+Range get_range(int size, int rank) {
   const auto dx = 2.0 / size;
-  const auto x_low = rank * dx - 1;
-  const auto x_high = (rank + 1) * dx - 1;
-  return std::make_pair(x_low, x_high);
+  const auto low = rank * dx - 1;
+  const auto high = (rank + 1) * dx - 1;
+  return {low, high};
 }
 
 int main(int argc, char* argv[]) {
@@ -43,10 +53,10 @@ int main(int argc, char* argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  const auto [x_low, x_high] = calc_my_range(size, rank);
+  const auto rng = get_range(size, rank);
   const int n = 1000;
-  const auto sum = integrate_using_trapezoidal_rule(
-      x_low, x_high, n, [](double x) { return 2 / (1 + x * x); });
+  const auto sum = integrate(
+      rng, n, [](double x) { return 2 / (1 + x * x); });
 
   double pi = 0.0;
   MPI_Reduce(&sum, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
